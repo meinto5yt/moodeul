d4f85da1942252bb9d963e4767ad3bf304d2c389
meinto5yt <kareemalmelege5@gmail.com>
2026-04-20 22:02:53 +0200

    Add files via upload

---
+++ vid/video.js
@@ -0,0 +1,12 @@
+var cartona = '<video src="Life on Land.mp4" autoplay controls></video>';
+var cartona2 = '<video src="About Growth Guiders Team.mp4" autoplay controls></video>';
+var cartona3 = '<video src="SDGs.mp4" autoplay controls></video>';
+function vid1() {
+    document.getElementById('vidAp').innerHTML = cartona;
+}
+function vid2() {
+    document.getElementById('vidAp').innerHTML = cartona2;
+}
+function vid3() {
+    document.getElementById('vidAp').innerHTML = cartona3;
+}
